506865ce63d08f020e6a89b16cf404cf38cfb1f3
Al Amin <ialamin.pro@gmail.com>
2022-04-29 23:09:21 +0600

    Login page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const loginRouter = require("./router/loginRuter");
 
 // internal imports
 const {
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
+app.use("/", loginRouter);
 
 // 404 not found handler
 app.use(notFoundHandler);
